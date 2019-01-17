78c1941f453d0bd95bee491a74aa2a5f60e64b0e
Jorge Martinez Hernandez <jorge@yonomi.co>
2019-01-16 21:02:56 -0600

    feat: Implement builders and marshalers
    Implement topic builders and payload marshalers/unmarshalers.
    
    TC-1364, TC-1365, TC-1366
    
    Signed-off-by: Jorge Martinez Hernandez <jorge@yonomi.co>

---
+++ Makefile
@@ -0,0 +1,70 @@
+#THIs target is to ensure accidental execution of Makefile as a bash script will not execute commands like rm in unexpected directories and exit gracefully.
+.prevent_execution:
+	exit 0
+
+CC = gcc
+
+#remove @ for no make command prints
+DEBUG = @
+
+GREATEST_DIR = ../greatest
+TEST_DIR = .
+TEST_INCLUDE_DIRS += -I $(TEST_DIR) -I $(GREATEST_DIR)
+TEST_NAME = tests 
+TEST_SRC_FILES = tests.c
+
+#IoT client directory
+IOT_CLIENT_DIR = ../aws-iot-device-sdk-embedded-C
+
+PLATFORM_DIR = $(IOT_CLIENT_DIR)/platform/linux/mbedtls
+PLATFORM_COMMON_DIR = $(IOT_CLIENT_DIR)/platform/linux/common
+
+IOT_INCLUDE_DIRS += -I $(IOT_CLIENT_DIR)/include
+IOT_INCLUDE_DIRS += -I $(IOT_CLIENT_DIR)/external_libs/jsmn
+IOT_INCLUDE_DIRS += -I $(PLATFORM_COMMON_DIR)
+IOT_INCLUDE_DIRS += -I $(PLATFORM_DIR)
+
+IOT_SRC_FILES += $(shell find $(IOT_CLIENT_DIR)/src -name '*.c')
+IOT_SRC_FILES += $(shell find $(IOT_CLIENT_DIR)/external_libs/jsmn -name '*.c')
+IOT_SRC_FILES += $(shell find $(PLATFORM_DIR) -name '*.c')
+IOT_SRC_FILES += $(shell find $(PLATFORM_COMMON_DIR) -name '*.c')
+
+#TLS - mbedtls
+MBEDTLS_DIR = $(IOT_CLIENT_DIR)/external_libs/mbedTLS
+TLS_LIB_DIR = $(MBEDTLS_DIR)/library
+TLS_INCLUDE_DIR = -I $(MBEDTLS_DIR)/include
+EXTERNAL_LIBS += -L$(TLS_LIB_DIR) 
+LD_FLAG += -Wl,-rpath,$(TLS_LIB_DIR) -ljson-c
+LD_FLAG += -ldl $(TLS_LIB_DIR)/libmbedtls.a $(TLS_LIB_DIR)/libmbedcrypto.a $(TLS_LIB_DIR)/libmbedx509.a -lpthread
+
+#Aggregate all include and src directories
+INCLUDE_ALL_DIRS += $(IOT_INCLUDE_DIRS)
+INCLUDE_ALL_DIRS += $(TLS_INCLUDE_DIR)
+INCLUDE_ALL_DIRS += $(TEST_INCLUDE_DIRS)
+
+SRC_FILES += $(TEST_SRC_FILES)
+SRC_FILES += $(IOT_SRC_FILES)
+
+# Logging level control
+LOG_FLAGS += -DENABLE_IOT_DEBUG
+LOG_FLAGS += -DENABLE_IOT_INFO
+LOG_FLAGS += -DENABLE_IOT_WARN
+LOG_FLAGS += -DENABLE_IOT_ERROR
+
+COMPILER_FLAGS += $(LOG_FLAGS) -g -DDEBUG
+#If the processor is big endian uncomment the compiler flag
+#COMPILER_FLAGS += -DREVERSED
+
+MBED_TLS_MAKE_CMD = $(MAKE) -C $(MBEDTLS_DIR)
+
+PRE_MAKE_CMD = $(MBED_TLS_MAKE_CMD)
+MAKE_CMD = $(CC) $(SRC_FILES) $(COMPILER_FLAGS) -o $(TEST_NAME) $(LD_FLAG) $(EXTERNAL_LIBS) $(INCLUDE_ALL_DIRS)
+
+all:
+	$(PRE_MAKE_CMD)
+	$(DEBUG)$(MAKE_CMD)
+	$(POST_MAKE_CMD)
+
+clean:
+	rm -f $(TEST_DIR)/$(TEST_NAME)
+	$(MBED_TLS_MAKE_CMD) clean
---
+++ tests.c
@@ -0,0 +1,274 @@
+#include <json-c/json.h>
+
+#include "thincloud.h"
+#include "greatest.h"
+
+SUITE(thincloud);
+
+TEST should_build_commission_topic(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+
+    const int rc = commission_request_topic(topic, "lock", "123456");
+
+    const char *expectedStr = "thincloud/registration/lock_123456/requests";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, topic);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_fail_commission_topic_on_null_inputs(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+
+    const int rc = commission_request_topic(topic, NULL, NULL);
+
+    ASSERT_EQ(NULL_VALUE_ERROR, rc);
+
+    PASS();
+}
+
+TEST should_build_commission_response_topic(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+    const int rc = commission_response_topic(topic, "lock", "123456", "123");
+
+    const char *expectedStr = "thincloud/registration/lock_123456/requests/123/response";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, topic);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_command_request_topic(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+    const int rc = command_request_topic(topic, "123456");
+
+    const char *expectedStr = "thincloud/devices/123456/command";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, topic);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_command_response_topic(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+    const int rc = command_response_topic(topic, "123456", "7890");
+
+    const char *expectedStr = "thincloud/devices/123456/command/7890/response";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, topic);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_service_request_topic(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+    const int rc = service_request_topic(topic, "123456");
+
+    const char *expectedStr = "thincloud/devices/123456/requests";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, topic);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_service_response_topic(void)
+{
+    char topic[MAX_TOPIC_LENGTH];
+    const int rc = service_response_topic(topic, "123456", "7890");
+
+    const char *expectedStr = "thincloud/devices/123456/requests/7890/response";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, topic);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_commission_request(void)
+{
+    char buffer[256];
+
+    const int rc = commissioning_request(buffer, "1234", "lock", "5678");
+
+    json_object *body = json_object_new_object();
+
+    const char *expectedStr = "{\"id\":\"1234\",\"method\":\"commission\",\"params\":{\"data\":{\"deviceType\":\"lock\",\"physicalId\":\"5678\"}}}";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, buffer);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_command_response(void)
+{
+    char buffer[256];
+
+    json_object *body = json_object_new_object();
+    json_object *bar = json_object_new_string("bar");
+    json_object_object_add(body, "foo", bar);
+
+    const int rc = command_response(buffer, "1234", 200, false, NULL, body);
+
+    const char *expectedStr = "{\"id\":\"1234\",\"result\":{\"statusCode\":200,\"body\":{\"foo\":\"bar\"}}}";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, buffer);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_build_service_request(void)
+{
+    char buffer[256];
+
+    json_object *params = json_object_new_object();
+    json_object *bar = json_object_new_string("bar");
+    json_object_object_add(params, "foo", bar);
+
+    const int rc = service_request(buffer, "1234", REQUEST_METHOD_GET, params);
+
+    const char *expectedStr = "{\"id\":\"1234\",\"method\":\"GET\",\"params\":{\"foo\":\"bar\"}}";
+    const size_t expectedStrLen = strlen(expectedStr);
+
+    ASSERT_STR_EQ(expectedStr, buffer);
+    ASSERT_EQ(expectedStrLen, rc);
+
+    PASS();
+}
+
+TEST should_process_commissioning_response(void)
+{
+    char deviceId[TC_ID_LENGTH] = {};
+    char requestId[TC_ID_LENGTH] = {};
+    uint16_t statusCode = 0;
+
+    const char *response = "{\"id\":\"1234\",\"result\":{\"statusCode\":200,\"deviceId\":\"5678\"}}";
+    const size_t respLen = strlen(response);
+
+    const IoT_Error_t rc = commissioning_response(deviceId, &statusCode, requestId, response, respLen);
+
+    ASSERT_EQ_FMT(rc, SUCCESS, "%d");
+
+    ASSERT_STR_EQ("5678", deviceId);
+    ASSERT_EQ(statusCode, 200);
+    ASSERT_STR_EQ("1234", requestId);
+
+    PASS();
+}
+
+TEST should_process_command_request(void)
+{
+    char requestId[TC_ID_LENGTH] = {};
+    char method[64] = {};
+
+    json_object *params = NULL;
+
+    const char *request = "{\"id\":\"1234\",\"method\":\"startRoutine\",\"params\":[{\"data\":{\"foo\":\"bar\"}}]}";
+
+    const IoT_Error_t rc = command_request(requestId, method, &params, request, strlen(request));
+
+    ASSERT_EQ_FMT(rc, SUCCESS, "%d");
+
+    ASSERT_STR_EQ("1234", requestId);
+    ASSERT_STR_EQ("startRoutine", method);
+
+    ASSERT(params != NULL);
+
+    json_object *foo = json_object_object_get(
+        json_object_object_get(
+            json_object_array_get_idx(params, 0),
+            "data"),
+        "foo");
+
+    ASSERT(foo != NULL);
+    ASSERT_STR_EQ("bar", json_object_get_string(foo));
+
+    json_object_put(foo);
+
+    PASS();
+}
+
+TEST should_process_service_response(void)
+{
+    char requestId[TC_ID_LENGTH] = {};
+    uint16_t statusCode = 0;
+
+    json_object *data = NULL;
+
+    const char *response = "{\"id\":\"1234\",\"result\":{\"statusCode\":200,\"body\":{\"foo\":\"bar\"}}}";
+
+    const IoT_Error_t rc = service_response(requestId, &statusCode, &data, response, strlen(response));
+
+    ASSERT_EQ_FMT(rc, SUCCESS, "%d");
+
+    ASSERT_STR_EQ("1234", requestId);
+    ASSERT_EQ(200, statusCode);
+
+    ASSERT(data != NULL);
+
+    json_object *foo = json_object_object_get(data, "foo");
+    ASSERT(foo != NULL);
+    ASSERT_STR_EQ("bar", json_object_get_string(foo));
+
+    json_object_put(foo);
+
+    PASS();
+}
+
+SUITE(tc_topics)
+{
+    RUN_TEST(should_build_commission_topic);
+    RUN_TEST(should_fail_commission_topic_on_null_inputs);
+    RUN_TEST(should_build_commission_response_topic);
+    RUN_TEST(should_build_command_request_topic);
+    RUN_TEST(should_build_command_response_topic);
+    RUN_TEST(should_build_service_request_topic);
+    RUN_TEST(should_build_service_response_topic);
+}
+
+SUITE(tc_unmarshal)
+{
+    RUN_TEST(should_build_commission_request);
+    RUN_TEST(should_build_command_response);
+    RUN_TEST(should_build_service_request);
+}
+
+SUITE(tc_marshal)
+{
+    RUN_TEST(should_process_commissioning_response);
+    RUN_TEST(should_process_command_request);
+    RUN_TEST(should_process_service_response);
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char **argv)
+{
+    GREATEST_MAIN_BEGIN();
+
+    RUN_SUITE(tc_topics);
+    RUN_SUITE(tc_unmarshal);
+    RUN_SUITE(tc_marshal);
+
+    GREATEST_MAIN_END();
+}
