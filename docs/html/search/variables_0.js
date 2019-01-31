4b8e43dffad82c9a36ee120432244456928ad2eb
Jorge Martinez <jmartinezhern@protonmail.com>
2019-01-31 18:45:52 +0000

    fix: Subscriptions would fail to invoke callbacks (#7)
    * fix: Subscriptions would fail to invoke callbacks
    
    Signed-off-by: Jorge Martinez Hernandez <jorge@yonomi.co>
    
    * chore: Bump version
    
    Signed-off-by: Jorge Martinez Hernandez <jorge@yonomi.co>

---
+++ docs/html/search/variables_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['command_5ftopic_5fbuffer',['COMMAND_TOPIC_BUFFER',['../thincloud_8h.html#adba43581442316165f362171ffb6e58f',1,'thincloud.h']]],
+  ['commissioning_5fresponse_5ftopic_5fbuffer',['COMMISSIONING_RESPONSE_TOPIC_BUFFER',['../thincloud_8h.html#a773f73b6891eec1154ce174c848f5d9a',1,'thincloud.h']]]
+];
---
+++ docs/html/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['service_5fresponse_5ftopic_5fbuffer',['SERVICE_RESPONSE_TOPIC_BUFFER',['../thincloud_8h.html#acbb5e06c666a2841616ac7cfb76946cb',1,'thincloud.h']]]
+];
